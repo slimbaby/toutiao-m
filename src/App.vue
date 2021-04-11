a4fb8e3e947ccbff5d05fa9388d356ab504d4dbf
Bean <460467982@qq.com>
2021-04-11 09:40:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="app">
+    1111
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
